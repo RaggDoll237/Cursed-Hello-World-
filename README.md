a29b6d3a7affd8e2639fea029bc85bfb0580ebe7
RaggDoll237 <raggdolly1337@protonmail.com>
2025-12-24 01:45:13 +0600

    Hello World!

---
+++ README.md
@@ -0,0 +1,3 @@
+#Hello World!
+---
+This is most cursed Hello World written by 80+ lines of code
---
+++ cursed_hello_world.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+int main(){
+int a = 1;
+
+    if (a == 1) {
+        while (a == 1) {
+            std::cout<<"H";
+            a++;
+        }
+    }
+int b = a;
+    if (b == 2) {
+        while (b == 2) {
+            std::cout<<"e";
+        b++;
+        }
+    }
+int c = b;
+    if (c == 3) {
+        while (c == 3) {
+            std::cout<<"l";
+        c++;
+        }
+    }
+int d = c;
+    if(d == 4){
+        while (d == 4) {
+        std::cout<<"l";
+        d++;
+        }
+    }
+int e = d;
+    if (e == 5) {
+        while (e == 5) {
+        std::cout<<"o";
+        e++;
+        }
+    }
+int f = e;
+    if (f == 6) {
+        while (f == 6) {
+            std::cout<<" ";
+        f++;
+        }
+    }
+int g = f;
+    if (g == 7) {
+        while (g == 7) {
+            std::cout<<"w";
+        g++;
+        }
+    }
+int h = g;
+    if (h == 8) {
+        while (h == 8) {
+            std::cout<<"o";
+        h++;
+        }
+    }
+int i = h;
+    if (i == 9) {
+        while (i == 9) {
+            std::cout<<"r";
+        i++;
+        }
+    }
+int j = i;
+    if (j == 10) {
+        while (j == 10) {
+            std::cout<<"l";
+        j++;
+        }
+    }
+int k = j;
+    if (k == 11) {
+        while (k == 11) {
+            std::cout<<"d";
+        k++;
+        }
+    }
+int l = k;
+    if (l == 12) {
+        while (l == 12) {
+            std::cout<<"!";
+        l++;
+        }
+    }
+}
